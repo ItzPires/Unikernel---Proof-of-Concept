bd040ac3cc9613fd7b7f7d2de8d63ef84d3d29b8
Samuel Pires <samueltapires@gmail.com>
2024-08-19 17:02:16 +0100

    Improving the Script to run Unikernel

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-qemu-system-x86_64 -kernel ../kernel/arch/x86_64/boot/bzImage -initrd ../Output/image.img -append "console=ttyS0" -nographic
+ERROR_FOUND=false
+
+KERNEL="../kernel/arch/x86_64/boot/bzImage"
+IMAGE="../Output/image.img"
+
+if [ ! -f "$KERNEL" ]; then
+    echo "Error: The Kernel does not exist."
+    ERROR_FOUND=true
+fi
+
+if [ ! -f "$IMAGE" ]; then
+    echo "Error: The Image file does not exist."
+    ERROR_FOUND=true
+fi
+
+if [ "$ERROR_FOUND" = true ]; then
+    echo "Run build.sh"
+    exit 1
+fi
+
+echo "Running Unikernel"
+qemu-system-x86_64 -kernel "$KERNEL" -initrd "$IMAGE" -append "console=ttyS0" -nographic
