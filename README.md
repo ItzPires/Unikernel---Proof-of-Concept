9090e8e779a03531239160e367ecf1b7a3218037
Samuel Pires <samueltapires@gmail.com>
2024-10-02 13:33:27 +0100

    Improve README; Implement support for VMWare

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Unikernel---Proof-of-Concept
-Unikernel Proof of Concept
+# Unikernel - Proof of Concept
+
+## What is a Unikernel?
+
+A Unikernel is a specialised image that contains only the parts of the operating system and libraries needed to run a specific application. Unlike traditional operating systems, which are general, they support a wide range of functionalities. Unikernels are highly minimalist, focused on providing only the essentials for a single purpose, which makes them lighter and more secure.
+
+## Motivation
+
+Based on the Unikernel concept and what currently exists, it was decided to use the Linux kernel to turn it into a Unikernel.
+
+This project was born from an idea explored in the context of a Master's thesis.
+
+## Installation Guide
+
+Follow the steps below to configure and run the project:
+
+### Requirements
+- Install the necessary development tools and libraries:
+```bash
+sudo apt-get update
+sudo apt install flex
+sudo apt install bison
+sudo apt install libelf-dev
+sudo apt install libssl-dev
+sudo apt install qemu
+```
+
+#### Clone the project repository
+```bash
+git clone https://github.com/ItzPires/Unikernel---Proof-of-Concept.git
+cd Unikernel---Proof-of-Concept
+git submodule update --init --recursive
+```
+
+#### Compilation
+-  To compile the Unikernel, run the following command:
+```bash
+./Scripts/build.sh <path_to_binary> [additional_parameters]
+```
+
+#### Execution
+- To run the Unikernel in QEMU:
+```bash
+./Scripts/run.sh -t qemu
+```
+- To run the Unikernel in VMWare:
+```bash
+./Scripts/run.sh -t vmware
+```
--- Scripts/run.sh
+++ Scripts/run.sh
@@ -5,6 +5,24 @@ ERROR_FOUND=false
 KERNEL="../kernel/arch/x86_64/boot/bzImage"
 IMAGE="../Output/image.img"
 
+usage() {
+    echo "Usage: $0 -t <target>"
+    exit 1
+}
+
+# Check number of arguments
+if [ "$#" -ne 2 ]; then
+    usage
+fi
+
+# Process arguments
+while getopts "t:" opt; do
+  case $opt in
+    t) TARGET="$OPTARG" ;;
+    *) usage ;;
+  esac
+done
+
 if [ ! -f "$KERNEL" ]; then
     echo "Error: The Kernel does not exist."
     ERROR_FOUND=true
@@ -20,5 +38,62 @@ if [ "$ERROR_FOUND" = true ]; then
     exit 1
 fi
 
-echo "Running Unikernel"
-qemu-system-x86_64 -kernel "$KERNEL" -initrd "$IMAGE" -append "console=ttyS0" -nographic
+if [ "$TARGET" = "qemu" ]; then
+    echo "Running Unikernel"
+    qemu-system-x86_64 -kernel "$KERNEL" -initrd "$IMAGE" -append "console=ttyS0" -nographic
+
+elif [ "$TARGET" = "vmware" ]; then
+echo "Building Unikernel to VMWare"
+IMAGE_SIZE_MB=100
+IMAGE_NAME="disk"
+IMAGE_DNAME="$IMAGE_NAME.img"
+MOUNT_DIR="/mnt/disk"
+
+echo "Criando imagem vazia de $IMAGE_SIZE_MB MB..."
+dd if=/dev/zero of=$IMAGE_DNAME bs=1M count=$IMAGE_SIZE_MB
+
+echo "Associando imagem ao dispositivo loop..."
+LOOP_DEVICE=$(sudo losetup -fP --show $IMAGE_DNAME)
+
+echo "Dispositivo associado: $LOOP_DEVICE"
+
+echo "Criando partição na imagem usando fdisk..."
+(
+echo n   # Adiciona uma nova partição
+echo p   # Partição primária
+echo 1   # Número da partição
+echo     # Primeiro setor (padrão)
+echo     # Último setor (padrão)
+echo w   # Escreve as alterações
+) | sudo fdisk $LOOP_DEVICE
+
+echo "Formatando a partição em ext4..."
+sudo mkfs.ext4 ${LOOP_DEVICE}p1
+
+echo "Criando diretório de montagem em $MOUNT_DIR..."
+sudo mkdir -p $MOUNT_DIR
+
+echo "Montando a partição ${LOOP_DEVICE}p1 em $MOUNT_DIR..."
+sudo mount ${LOOP_DEVICE}p1 $MOUNT_DIR
+
+echo "Instalando o GRUB no dispositivo loop..."
+sudo grub-install --boot-directory=$MOUNT_DIR/boot $LOOP_DEVICE
+
+echo "Copiando arquivos de kernel e imagem para o diretório de boot..."
+sudo cp $KERNEL $MOUNT_DIR/boot/
+sudo cp $IMAGE $MOUNT_DIR/boot/
+
+echo "Criando o arquivo grub.cfg..."
+cat <<EOF > $MOUNT_DIR/boot/grub/grub.cfg
+menuentry "Unikernel" {
+    linux /boot/bzImage
+    initrd /boot/image.img
+}
+EOF
+
+sudo umount /mnt/disk
+
+qemu-img convert -f raw -O vmdk $IMAGE_DNAME "../Output/$IMAGE_NAME".vmdk
+
+rm -r $IMAGE_DNAME
+fi
